8ad805670eb3f2fdeaafb8667f66b63ccb892114
emersonvergara <emerson.vergara@ceiba.com.co>
2021-08-17 15:20:12 -0500

    Inyección de JS en HTML Para inyectar el código dinámico que genera Webpack en los archivos HTML, necesita 2 dependencias : html-webpack-plugin y html-loader. se instalan las depencias: npm i -D html-webpack-plugin html-loader
    Ademas debemos decirle a webpack que queremos inyectar el js a un hrml y para eso debemos crear la regla que nos permita realizar dicha accion.
    {
    	test: /\.html$/i,
    	use: [
    	  {
    		loader: "html-loader",
    		options: { minimize: true },
    	  },
    	],
     },
    
     mas el plugging
      plugins: [
        new HtmlWebpackPlugin({
          template: "./src/index.html",
          filename: "./index.html",
        }),
      ],
    
     Ahora crea el archivo ./src/index.html:

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.js$/i,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-          },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-      ],
-    },
-  };
+      },
+      {
+        test: /\.html$/i,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
